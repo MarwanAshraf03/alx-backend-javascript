c239e96d3d9d5fa5832f74f7c4d19b7879800481
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-01 02:16:51 +0300

    12

---
+++ 0x00-ES6_basic/12-main.js
@@ -0,0 +1,14 @@
+import createEmployeesObject from './11-createEmployeesObject';
+import createReportObject from './12-createReportObject';
+
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+};      
+
+// console.log(...employees);
+
+
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));
