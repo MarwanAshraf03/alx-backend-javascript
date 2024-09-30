ad09cf1eb0874e6decc548d8bf5e1083555ab95e
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-01 02:31:33 +0300

    1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,9 +2,7 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse && 0) {
-    // let task = true;
-    // let task2 = false;
+  if (trueOrFalse) {
     return [task, task2];
   }
   return [task, task2];
