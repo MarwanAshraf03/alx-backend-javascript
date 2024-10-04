daec1b89e610d7eb5bf485fcd19bc893dd3eccd7
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-04 23:50:21 +0300

    9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,52 +1,53 @@
 // Define the HolbertonClass first, before using it
 export class HolbertonClass {
-	constructor(year, location) {
+  constructor(year, location) {
 	  this._year = year;
 	  this._location = location;
-	}
-  
-	get year() {
-	  return this._year;
-	}
-  
-	get location() {
-	  return this._location;
-	}
   }
-  
-  // Now we can create instances of HolbertonClass
-  const class2019 = new HolbertonClass(2019, 'San Francisco');
-  const class2020 = new HolbertonClass(2020, 'San Francisco');
-  
-  // Define the StudentHolberton class
-  export class StudentHolberton {
-	constructor(firstName, lastName, holbertonClass) {
+
+  get year() {
+	  return this._year;
+  }
+
+  get location() {
+	  return this._location;
+  }
+}
+
+// Now we can create instances of HolbertonClass
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+// Define the StudentHolberton class
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
 	  this._firstName = firstName;
 	  this._lastName = lastName;
 	  this._holbertonClass = holbertonClass; // Set the holbertonClass in the constructor
-	}
-  
-	get fullName() {
+  }
+
+  get fullName() {
 	  return `${this._firstName} ${this._lastName}`;
-	}
-  
-	// Fix the getter to return the internal variable _holbertonClass
-	get holbertonClass() {
+  }
+
+  // Fix the getter to return the internal variable _holbertonClass
+  get holbertonClass() {
 	  return this._holbertonClass;
-	}
-  
-	get fullStudentDescription() {
+  }
+
+  get fullStudentDescription() {
 	  // Use `this` instead of `self`
 	  return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-	}
   }
-  
-  // Now we can create instances of StudentHolberton
-  const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-  const student2 = new StudentHolberton('John', 'Doe', class2020);
-  const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-  const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-  const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-  
-  // Export the list of students
-  export const listOfStudents = [student1, student2, student3, student4, student5];
+}
+
+// Now we can create instances of StudentHolberton
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+// Export the list of students
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
