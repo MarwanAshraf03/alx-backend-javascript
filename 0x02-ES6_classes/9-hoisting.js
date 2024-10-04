f226d98baf94d59e1a661081f8571f402c90f790
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-04 22:53:01 +0300

    9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -15,27 +15,25 @@ class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-
-
 const StudentHolberton = class StudentHolberton {
-	constructor(firstName, lastName, holbertonClass) {
-		this._firstName = firstName;
-		this._lastName = lastName;
-		this._holbertonClass = holbertonClass;
-	}
-	
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
   get fullName() {
-	  return `${this._firstName} ${this._lastName}`;
-	}
-	
-	get holbertonClass() {
-		return this.holbertonClass;
-	}
-	
-	get fullStudentDescription() {
-		return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-	}
-}
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+};
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
