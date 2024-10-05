a3b117c50c5fb4014b9c023fe730a965b7f71b23
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-05 04:02:21 +0300

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((values) => console.log(values[0].body, values[1].firstName, values[1].lastName)).catch(() => { console.log('Signup system offline'); });
+  return new Promise((resolve, reject) => {
+    Promise.all([uploadPhoto(), createUser()])
+      .then((values) => {
+        resolve(console.log(values[0].body, values[1].firstName, values[1].lastName));
+      })
+      .catch(() => { reject(console.log('Signup system offline')); });
+  });
 }
