25d93497facbe7aa931bd5ac9a47f27fbebf6477
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-05 05:28:02 +0300

    100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,8 +2,16 @@ import { createUser, uploadPhoto } from './utils';
 
 export default async function asyncUploadUser() {
   const results = [];
-  results.push(await uploadPhoto());
-  results.push(await createUser());
+  try {
+    results.push(await uploadPhoto());
+  } catch (Error) {
+    results.push(null);
+  }
+  try {
+    results.push(await createUser());
+  } catch (Error) {
+    results.push(null);
+  }
   return {
     photo: results[0],
     user: results[1],
