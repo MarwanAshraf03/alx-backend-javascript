d2cbbb6e81fa4286db9b3c5a0b77076209a4ff75
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-05 04:44:20 +0300

    6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,16 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function (firstName, lastName , fileName) {
-	
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  let upload;
+  const signup = await signUpUser(fileName, lastName);
+  //   console.log(signup);
+  try {
+    upload = await uploadPhoto(fileName);
+    // console.log(upload);
+  } catch (Error) {
+    // console.log("bomb!")
+
+  }
+  return { signup, upload };
 }
