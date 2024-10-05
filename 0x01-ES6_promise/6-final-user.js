89330b0e4921181af7982ff189bbb6e0082e97e8
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-05 04:51:45 +0300

    6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  let upload;
-  let signup = await signUpUser(fileName, lastName);
-  signup = { status: 200, value: signup };
-  //   console.log(signup);
-  try {
-    upload = await uploadPhoto(fileName);
-    upload = { status: 200, value: upload };
-    // console.log(upload);
-  } catch (Error) {
-    upload = { status: 404, value: Error };
-    // console.log("bomb!")
-  }
-  return [signup, upload];
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
