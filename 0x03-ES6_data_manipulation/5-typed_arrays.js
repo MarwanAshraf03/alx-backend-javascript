652d0fa46232a74964c66030a3d41beab7aeb872
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-09 02:25:53 +0300

    5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position > length || position < length) throw new Error('Position outside range');
+  const arr = new Int8Array(length);
+  try {
+    arr[position] = value;
+  } catch (Error) {
+    throw Error('Position outside range');
+  }
+  return arr;
+}
