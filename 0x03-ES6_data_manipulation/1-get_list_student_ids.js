504c0a3ae09ef6832ef1dbe89c3703b0fd2ca0f1
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-09 01:38:31 +0300

    1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,4 @@
 export default function getListStudentIds(list) {
   if (typeof list !== 'object') return [];
-  const ret = [];
-  list.map((obj) => ret.push(obj.id));
-  return ret;
+  return list.map(obj => obj.id);
 }
