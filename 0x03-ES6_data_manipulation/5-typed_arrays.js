60968543686665ef82b95d3c7dde9377a4b9238c
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-09 02:46:17 +0300

    5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,28 +1,9 @@
-// export default function createInt8TypedArray(length, position, value) {
-	//   if (position > length || position < length) throw new Error('Position outside range');
-	//   const arr = new Int8Array(length);
-	//   try {
-		//     arr[position] = value;
-		//   } catch (Error) {
-			//     throw new Error('Position outside range');
-			//   }
-//   return arr;
-// }
-
 export default function createInt8TypedArray(length, position, value) {
-	const uint8 = new Uint8Array([1, 2, 3]);
-console.log(uint8[0]); // 1
-
-// For illustrative purposes only. Not for production code.
-uint8[-1] = 0;
-uint8[2.5] = 0;
-uint8[NaN] = 0;
-console.log(Object.keys(uint8)); // ["0", "1", "2"]
-console.log(uint8[NaN]); // undefined
-
-// Non-numeric access still works
-uint8[true] = 0;
-console.log(uint8[true]); // 0
-
-Object.freeze(uint8); // TypeError: Cannot freeze array buffer views with 
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  view.setInt8(position, value);
+  return view;
 }
