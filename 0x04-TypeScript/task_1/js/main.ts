04e952bba8b7537cc693164411e495f35a8fc23a
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-12 06:01:47 +0300

    task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,12 +7,44 @@ interface Teacher {
 	[attrName: string]: unknown
 }
 
-const teacher3: Teacher = {
-	firstName: 'John',
-	fullTimeEmployee: false,
-	lastName: 'Doe',
-	location: 'London',
-	contract: false,
-  };
-  
-  console.log(teacher3);
+interface Directors extends Teacher {
+	numberOfReports: number
+}
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string
+}
+
+const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string{
+	return `${firstName[0]}. ${lastName}`
+}
+
+interface StudentClassConstructor {
+	(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+	firstName: string;
+	lastName: string;
+	workOnHomework(): string;
+	displayName(): string;
+
+}
+
+class StudentClass {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string{
+		return "Currently working"
+	}
+
+	displayName(): string{
+		return `${this.firstName}`
+	}
+}
