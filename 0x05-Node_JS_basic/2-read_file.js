c6307c3428345125230423ecda88a1ae982c65e6
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-12-02 20:55:49 +0200

    2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function csvParser(data) {
   const datat = {};
   const strList = data;
-  const colsList = data[0].split(",");
+  const colsList = data[0].split(',');
   for (let i = 1; i < strList.length; i += 1) {
     datat[i] = {};
     for (let j = 0; j < colsList.length; j += 1) {
-      datat[i][colsList[j]] = data[i].split(",")[j];
+      datat[i][colsList[j]] = data[i].split(',')[j];
     }
   }
   return datat;
@@ -15,13 +15,12 @@ function csvParser(data) {
 
 function countStudents(path) {
   try {
-    let data = fs.readFileSync(path, { encoding: "utf8", flag: "r" });
-    data = data.split("\n").filter((n) => n);
+    let data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+    data = data.split('\n').filter((n) => n);
     const datat = csvParser(data);
     const dataByField = {};
     Object.values(datat).forEach((e) => {
-      if (!Object.keys(dataByField).includes(e.field))
-        dataByField[e.field] = [];
+      if (!Object.keys(dataByField).includes(e.field)) dataByField[e.field] = [];
       dataByField[e.field].push(e.firstname);
     });
     console.log(`Number of students: ${data.length - 1}`);
@@ -29,11 +28,11 @@ function countStudents(path) {
       console.log(
         `Number of students in ${e}: ${
           dataByField[e].length
-        }. List: ${dataByField[e].join(", ")}`
+        }. List: ${dataByField[e].join(', ')}`,
       );
     });
   } catch (ENOENT) {
-    console.log("Cannot load the database");
+    console.log('Cannot load the database');
   }
 }
 module.exports = countStudents;
