fb8cc6bdb150523b52251d6c51f5fdfdfd4f3b19
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-12-10 15:22:18 +0200

    4+

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const http = require("http");
 
-const app = http
+let app = http
   .createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello ALX!');
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello ALX!");
   })
   .listen(1245);
 
