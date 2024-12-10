3d67d1407838a336c719cd7fdba97405afdcc86c
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-12-10 15:25:18 +0200

    4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,17 @@
-const http = require("http");
+const http = require('http');
 
-let app = http
+const app = http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello ALX!");
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello ALX!');
+    } else if (req.url === '/test') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Test route!');
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    }
   })
   .listen(1245);
 
