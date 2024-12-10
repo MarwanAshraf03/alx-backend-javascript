abf46c299a74f57ac61a11f37cb2e79ff07a39e6
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-12-10 16:26:53 +0200

    5

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,17 +1,9 @@
-const http = require('http');
+const http = require("http");
 
 const app = http
   .createServer((req, res) => {
-    if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello ALX!');
-    } else if (req.url === '/test') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Test route!');
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found');
-    }
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello ALX!");
   })
   .listen(1245);
 
---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const fs = require('fs');
+
+function csvParser(data) {
+  const datat = {};
+  const strList = data;
+  const colsList = data[0].split(',');
+  for (let i = 1; i < strList.length; i += 1) {
+    datat[i] = {};
+    for (let j = 0; j < colsList.length; j += 1) {
+      datat[i][colsList[j]] = data[i].split(',')[j];
+    }
+  }
+  return datat;
+}
+
+function countStudents(path) {
+  let str = '';
+  try {
+    let data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+    data = data.split('\n').filter((n) => n);
+    const datat = csvParser(data);
+    const dataByField = {};
+    Object.values(datat).forEach((e) => {
+      if (!Object.keys(dataByField).includes(e.field)) dataByField[e.field] = [];
+      dataByField[e.field].push(e.firstname);
+    });
+    str += `Number of students: ${data.length - 1}`;
+    Object.keys(dataByField).forEach((e) => {
+      str += `\nNumber of students in ${e}: ${
+        dataByField[e].length
+      }. List: ${dataByField[e].join(', ')}`;
+    });
+
+    return str;
+  } catch (ENOENT) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
+
+const app = http
+  .createServer((req, res) => {
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello ALX!');
+    }
+    if (req.url === '/students') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(countStudents('database.csv'));
+    }
+  })
+  .listen(1245);
+
+module.exports = app;
